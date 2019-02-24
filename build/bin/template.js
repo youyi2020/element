bba1ddcb2e0b0f5b4778d93141d9082d8d51ebe2
“your_youyi.sun <youyi.sun@aispeech.com>
2019-02-24 16:08:25 +0800

    bin/template.js注解

--- build/bin/template.js
+++ build/bin/template.js
@@ -1,7 +1,9 @@
+// 当examples/pages/template文件发生改变时候 会重新执行npm run i18h生产新的page页面
+// 注意:开发模式下(dev和dev:play) 不执行此文件
 const path = require('path');
 const templates = path.resolve(process.cwd(), './examples/pages/template');
 
-const chokidar = require('chokidar');
+const chokidar = require('chokidar'); // 基于node.JS的监听文件夹改变模块
 let watcher = chokidar.watch([templates]);
 
 watcher.on('ready', function() {
