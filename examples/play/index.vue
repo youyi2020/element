909135c22bb7d6ed9922cf0acad5953eeb05859c
“your_youyi.sun <youyi.sun@aispeech.com>
2019-04-30 19:25:18 +0800

    ~

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div style="margin: 20px;">
-    <!--<el-button type="success" round>你好</el-button>-->
+    <el-button type="success" round>你好</el-button>
     <!--<el-btn size="small" round @click="handleClick">世界</el-btn>-->
     <!--<el-button-group>-->
         <!--<el-button type="success" round>你好</el-button>-->
         <!--<el-button type="success" round>你好</el-button>-->
         <!--<el-button type="success" round>你好</el-button>-->
     <!--</el-button-group>-->
-    <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple">1</div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple">2</div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple">3</div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple">4</div></el-col>
-    </el-row>
+    <el-container>
+        <el-header>1</el-header>
+        <el-footer>2</el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -22,6 +20,9 @@
       return {
         input: 'Hello Element UI!'
       };
+    },
+    mounted(){
+
     },
     methods : {
       handleClick(evt){
--- packages/container/src/main.vue
+++ packages/container/src/main.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+  // componentName 自定义属性，其他组件会用到
   export default {
     name: 'ElContainer',
 
@@ -16,13 +17,19 @@
 
     computed: {
       isVertical() {
+        console.log(this.$slots.default);
+        console.log(this.$slots);
         if (this.direction === 'vertical') {
           return true;
         } else if (this.direction === 'horizontal') {
           return false;
         }
+        // this.$slots.default返回了所有没有被包含在具名插槽中的子元素
+        // some高阶函数的使用，这里就是要在所有子元素中查看是否存在<el-header>或者<el-footer>,some所遍历的数组只要有一个为true，则整体返回true
+        // 而every则是要所有都是true才返回true
         return this.$slots && this.$slots.default
           ? this.$slots.default.some(vnode => {
+            // VNode是虚拟dom中的虚拟节点，当组件被编译时，每个<...>就会生成一个虚拟的节点
             const tag = vnode.componentOptions && vnode.componentOptions.tag;
             return tag === 'el-header' || tag === 'el-footer';
           })
